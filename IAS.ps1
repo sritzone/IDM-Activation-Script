960ed1370b2181a769529fea1ad56e9187b6b37b
SR IT ZONE <sritzoneofficial@gmail.com>
2026-08-08 11:11:32 +0600

    Add files via upload

---
+++ IAS.ps1
@@ -0,0 +1,25 @@
+# Enable TLSv1.2 for security
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Corrected RAW file URL
+$DownloadURL = 'https://raw.githubusercontent.com/sritzone/IDM-Activation-Script/main/IAS.cmd'
+
+# Generate a random filename
+$rand = Get-Random -Maximum 99999999
+$isAdmin = [bool]([Security.Principal.WindowsIdentity]::GetCurrent().Groups -match 'S-1-5-32-544')
+$FilePath = if ($isAdmin) { "$env:SystemRoot\Temp\IAS_$rand.cmd" } else { "$env:TEMP\IAS_$rand.cmd" }
+
+# Try downloading the file
+try {
+    $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+} catch {
+    Write-Host "Failed to download the script. Check the URL!"
+    exit
+}
+
+# Save the script and execute it
+Set-Content -Path $FilePath -Value $response.Content
+Start-Process -FilePath $FilePath -Wait
+
+# Cleanup
+Remove-Item -Path $FilePath -Force
